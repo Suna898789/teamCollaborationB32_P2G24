fbde5f7da66b549c0c13494183b71513fe96e708
Suna898789 <HASNAE05@live.fr>
2024-01-31 11:43:54 -0500

    Add precondition for negative number

--- src/week1/OddEven_has.java
+++ src/week1/OddEven_has.java
@@ -4,7 +4,10 @@ public class OddEven_has {
 
 
     public static void identify(int number){
-
+if (number<0){
+    System.err.println("Negative number!!!!!");
+    System.exit(1);
+}
         if(number%2==0){
             System.out.println(number+ " is an even number");
         }else{
