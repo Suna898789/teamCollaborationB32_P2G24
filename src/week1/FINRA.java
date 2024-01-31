930a54c11bbda4b8540f66df2d274902df707a26
Kelyxa <kelyxamax@gmail.com>
2024-01-30 19:54:18 -0500

    Created FINRA java file

---
+++ src/week1/FINRA.java
@@ -0,0 +1,21 @@
+public static void finra(int n){
+        for (int i = 0; i <=n; i++) {
+
+        if (i%3==0 && i%5==0){
+        System.out.println("FINRA");
+        }else if(i%3==0){
+        System.out.println("FIN");
+        } else if (i%5==0) {
+        System.out.println("RA");
+        }else{
+        System.out.println(i);
+        }
+        }
+        }
+
+public static void main(String[] args) {
+        finra(30);
+        }
+
+
+        }
