c737d05ddfe3eceae82d698034d503e477994171
lunaoll <luna@cydeo.com>
2024-02-01 16:32:47 +0300

    add week1 solutions

---
+++ src/main/java/luna/week1/Q1.java
@@ -0,0 +1,14 @@
+package luna.week1;
+
+public class Q1 {
+
+    public static void main(String[] args) {
+        System.out.println(identifyOddEven(4));
+        System.out.println(identifyOddEven(5));
+    }
+
+    public  static  String  identifyOddEven( int  n ) {
+        return  n%2==0 ? "Even" : "Odd" ;
+    }
+
+}
---
+++ src/main/java/luna/week1/Q3.java
@@ -0,0 +1,23 @@
+package luna.week1;
+
+public class Q3 {
+    public static void main(String[] args) {
+        finra();
+    }
+
+    public static void finra() {
+        String result = "";
+        for(int i=1; i <= 30; i++) {
+            if(i % 5==0 && i %3 ==0)
+                result += "FINRA ";
+            else if(i%5 == 0)
+                result += "RA ";
+            else if(i%3==0)
+                result+="FIN ";
+            else
+                result += i+" ";
+        }
+        System.out.println(result);
+    }
+
+}
