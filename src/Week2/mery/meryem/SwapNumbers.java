fa58c168ef27713d42dc60c83920511827248cd1
maryemamstine <amstinemeryem77@gmail.com>
2024-02-02 15:09:51 -0500

    created a SwapNumbers class

---
+++ src/Week2/mery/meryem/SwapNumbers.java
@@ -0,0 +1,45 @@
+//** created by Maryam 
+
+package Week2.mery.meryem;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 15;
+        //Logic 1 - use + & - without using third variable;
+/*
+        a = a + b;//  5 + 15 = 20 update
+        b = a - b; //20 - 15 =  5
+        a = a - b;// 20 - 5 = 15
+
+
+
+        System.out.println("b = " + b);// 5
+        System.out.println("a = " + a);//15
+
+ */
+
+        System.out.println("================================================");
+
+        //Logic 2 use * & / without using third variable;
+        // a & b values should not be zero
+        if ( a == 0 && b == 0) {
+            throw new RuntimeException("a & b values should not be zero");
+        }
+
+            a = a * b;//75
+            b = a / b;//75 / 15 =5
+            a = a / b;// 75 / 5 =15
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+
+    }
+
+}
+/*
+Swap two variable values without using a third variable
+ */
