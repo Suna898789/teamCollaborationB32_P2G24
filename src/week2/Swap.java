2f955c3a22f4177f92ab7f0204f47dc6080cae61
Shohruh0312 <bshohruh92@gmail.com>
2024-02-06 09:26:26 -0500

    add swap task

---
+++ src/week2/Swap.java
@@ -0,0 +1,20 @@
+package week2;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        
+        int a = 20;
+        int b = 5;
+        
+        a = a + b; // a = 25
+        
+        b = a - b; // b = 5
+
+        a = a - b;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
+}
