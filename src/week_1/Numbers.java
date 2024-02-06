8ddcc2905d68ddeda868265db7b5f0142bcefbb4
Ivan <kinca30.03@gmail.com>
2024-02-05 19:29:31 -0500

    week1

--- src/week_1/Numbers.java
+++ src/week_1/Numbers.java
@@ -1,14 +1,4 @@
 package week_1;
 
 public class Numbers {
-    public static void main(String[] args) {
-
-        int number=34;
-
-        if (number%2==0){
-            System.out.println(number+" is Even");
-        }else System.out.println(number+" is Odd");
-    }
-
-
 }
