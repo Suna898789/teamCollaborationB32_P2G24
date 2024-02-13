e27dec91edc9412b464aa378aaf3dacc00b2a61e
Ivan <kinca30.03@gmail.com>
2024-02-13 11:43:39 -0500

    Upload week3

---
+++ src/Ivan/week_2/Swap.java
@@ -0,0 +1,21 @@
+package Ivan.week_2;
+
+public class Swap {
+    public static void main(String[] args) {
+        int n1=2;
+        int n2=5;
+
+         n1=n2+n1;
+
+         n2=n1-n2;
+
+         n1=n1-n2;
+
+        System.out.println("n1 = " + n1);
+
+        System.out.println("n2 = " + n2);
+
+
+
+    }
+}
