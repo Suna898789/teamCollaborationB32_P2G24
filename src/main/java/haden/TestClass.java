513cebc30370cbce14e4b03371d1e58b69f009f5
Haden <haden@cydeo.com>
2024-02-20 22:09:06 +0300

    TestClass has been added

---
+++ src/main/java/haden/TestClass.java
@@ -0,0 +1,12 @@
+package main.java.haden;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        // New codes for the project.
+
+        // I have typed some codes
+
+    }
+}
